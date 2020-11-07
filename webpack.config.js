9dd7eabaaa26dad1e77c79e9ba3e5eb0d5e79484
Tyler <tylerschloesser@gmail.com>
2020-11-06 19:49:34 -0800

    webpack async

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
-      },
+module.exports = async env => {
+  console.log('webpack config')
+  return {
+    entry: './src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  stats: 'minimal',
+    stats: 'minimal',
+  }
 }
