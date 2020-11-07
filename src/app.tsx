8b5b8b685134cf6e4025f29b5ec469f91d9ad776
Tyler <tylerschloesser@gmail.com>
2020-11-06 20:35:47 -0800

    cp

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,42 @@
 import * as React from 'react'
 import { useEffect, useState, useRef } from 'react'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom"
 
 import './app.scss'
+
+interface AppParams {
+  tool: string
+}
+
+const Tool = () => {
+  const { tool } = useParams<AppParams>()
+  return <div>{tool}</div>
+}
 
 export const App = () => {
   useEffect(() => {
     console.log('main')
   }, [])
   return (
-    <div className="app">
-      <div className="app-a1">
-        <h1>
-          <a href="#">
-            environment
-          </a>
-        </h1>
+    <Router hashType="noslash">
+      <Switch>
+        <Route path="/:tool" component={Tool} />
+      </Switch>
+      <div className="app">
+        <div id="environment" className="app-a1">
+          <h1>
+            <a href="#environment">
+              environment
+            </a>
+          </h1>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
