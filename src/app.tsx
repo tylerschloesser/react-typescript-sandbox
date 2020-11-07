5b4149061c6e052c4623993e3a23a66e3824e8d4
Tyler <tylerschloesser@gmail.com>
2020-11-06 20:14:42 -0800

    cp

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,18 @@ import { useEffect, useState, useRef } from 'react'
 import './app.scss'
 
 export const App = () => {
+  useEffect(() => {
+    console.log('main')
+  }, [])
   return (
     <div className="app">
-      hi
+      <div className="app-a1">
+        <h1>
+          <a href="#">
+            environment
+          </a>
+        </h1>
+      </div>
     </div>
   )
 }
