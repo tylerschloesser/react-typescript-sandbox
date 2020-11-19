b36c8ce419f4d8baac36b026efd4eb525f63f0bf
Tyler Schloesser <tschloes@amazon.com>
2020-11-18 19:28:40 -0800

    cp

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,14 @@ import { useEffect, useState, useRef } from 'react'
 import './app.scss'
 
 export const App = () => {
+
+  const bem = (block, modifiers: string[] = []) => ([
+    block,
+    ...modifiers.map(m=>`${block}--${m}`),
+  ].join(' '))
+
   return (
-    <div className="app">
+    <div className={bem('app')}>
       hi
     </div>
   )
