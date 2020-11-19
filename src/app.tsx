def7c46ee6aa09a107aa84f0c59451d98155f7d3
Tyler Schloesser <tschloes@amazon.com>
2020-11-18 20:40:11 -0800

    cp

--- src/app.tsx
+++ src/app.tsx
@@ -18,12 +18,17 @@ export const App = () => {
   const alt = ""
 
   return (
-    <div className="story-detail-page">
-      <div className="__header">todo header</div>
-      <div className="__image">
-        <img src={src} alt={alt} />
+    <>
+      <div className="story-detail-page">
+        <div className="__header">todo header</div>
+        <div className="__image">
+          <img src={src} alt={alt} />
+        </div>
+        <div className="__crown" />
+        <h1 className="__title">
+          {`Tea Cups and Train Wrecks`}
+        </h1>
       </div>
-      <div className="__crown" />
-    </div>
+    </>
   )
 }
