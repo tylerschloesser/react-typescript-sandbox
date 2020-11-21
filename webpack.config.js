c3ae87be1ed658db46704fac2aba7cb1dad3af5b
Tyler Schloesser <tschloes@amazon.com>
2020-11-21 08:38:01 -0800

    local crown

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,14 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+        use: [ 
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+          'sass-loader' 
+        ],
       },
     ],
   },
