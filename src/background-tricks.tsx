96194b2cbbc1f087abdab6a3600f6e83fafaac82
Tyler Schloesser <tschloes@amazon.com>
2020-12-09 13:32:27 -0800

    cp

---
+++ src/background-tricks.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import './background-tricks.scss'
+
+export const BackgroundTricks = () => {
+  return (
+    <div className="background-tricks">
+      <div className="radial-gradient" />
+    </div>
+  )
+}
